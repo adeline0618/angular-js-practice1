528e5475dec1bf08ffc5d64ef836430e65819794
Swan <adeliine.0618@gmail.com>
2023-05-16 13:52:09 +0900

    Feat : add todo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Angular.JS를 익히기 위한 레포지토리입니다.
 
 [공식사이트](https://docs.angularjs.org/guide/concepts)
+
+# angular.js 1.4.4와 bootstrap을 이용하여 아주 간단한 투두 리스트를 구현
--- src/script.js
+++ src/script.js
@@ -31,6 +31,20 @@
         alert('삭제된 내용은 되돌릴 수 없습니다.');
         $scope.todos = $scope.todos.filter(el => el.title !== todo.title);
       };
+
+      $scope.add = function (newTodoTitle) {
+        if ($scope.todos.filter(el => el.title === newTodoTitle).length) {
+          alert('이미 존재하는 항목입니다.');
+          $scope.newTodoTitle = '';
+          return;
+        }
+        $scope.todos.unshift({
+          title: newTodoTitle,
+          completed: false,
+          createdAt: Date.now(),
+        });
+        $scope.newTodoTitle = '';
+      };
     },
   ]);
 })();
