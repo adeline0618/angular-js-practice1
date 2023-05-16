ffbaa6d6b861ee25bec4d774be876e784498e5c2
Swan <adeliine.0618@gmail.com>
2023-05-16 14:46:28 +0900

    Feat : directive 활욜 filters,form

--- src/directive.js
+++ src/directive.js
@@ -1,10 +1,20 @@
-angular.module('todo').directive('todoTitle', function () {
+angular.module('Todo').directive('todoTitle', function () {
   return {
     template: '<h1>Angular.js Todo List</h1>',
   };
 });
-angular.module('todo').directive('todoItem', function () {
+angular.module('Todo').directive('todoItem', function () {
   return {
     templateUrl: './template/todoItem.tpl.html',
+  };
+});
+angular.module('Todo').directive('todoFilters', function () {
+  return {
+    templateUrl: './template/todoFilters.tpl.html',
+  };
+});
+angular.module('Todo').directive('todoForm', function () {
+  return {
+    templateUrl: './template/todoForm.tpl.html',
   };
 });
